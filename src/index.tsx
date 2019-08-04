c4c8c5f0e4d2d6d5fb0051781ba2028d05e02731
Annie-Huang <annie.hn@gmail.com>
2019-08-04 20:41:40 +1000

    Make it workable in IE11

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 // tslint:disable-next-line
 import 'bootstrap/dist/css/bootstrap.css';
+// tslint:disable-next-line
+import 'react-app-polyfill/ie11';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
